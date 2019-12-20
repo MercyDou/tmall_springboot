e1b6d17ebb9bfe3d36d2878d32322131b5bbd007
MercyDou <2983055254@qq.com>
2019-12-20 19:11:41 +0800

    分类管理完成

--- src/main/java/com/tmall/web/AdminPageController.java
+++ src/main/java/com/tmall/web/AdminPageController.java
@@ -1,10 +1,14 @@
 package com.tmall.web;
 
+import com.tmall.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
 public class AdminPageController {
+    @Autowired
+    CategoryService categoryService;
+
     @GetMapping(value = "/admin")
     public String admin() {
         return "redirect:admin_category_list";
@@ -14,4 +18,10 @@ public class AdminPageController {
     public String listCategory() {
         return "admin/listCategory";
     }
+
+    @GetMapping(value = "admin_category_edit")
+    public String editCategory() {
+        return "admin/editCategory";
+    }
 }
+
--- src/main/java/com/tmall/web/CategoryController.java
+++ src/main/java/com/tmall/web/CategoryController.java
@@ -4,15 +4,12 @@ import com.tmall.pojo.Category;
 import com.tmall.service.CategoryService;
 import com.tmall.util.ImageUtil;
 import com.tmall.util.Page4Navigator;
+import org.hibernate.event.spi.SaveOrUpdateEvent;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -48,5 +45,34 @@ public class CategoryController {
         image.transferTo(file);
         BufferedImage img = ImageUtil.change2jpg(file);
         ImageIO.write(img, "jpg", file);
+    }
+
+    @DeleteMapping("/categories/{id}")
+    public String delete(@PathVariable("id") int id, HttpServletRequest request) throws Exception {
+        categoryService.delete(id);
+        File imageFolder = new File(request.getServletContext().getRealPath("img/category"));
+        File file = new File(imageFolder, id + ".jpg");
+        file.delete();
+        return null;
+    }
+
+    @GetMapping("/categories/{id}")
+    public Category get(@PathVariable("id")int id)throws Exception {
+        Category bean = categoryService.get(id);
+        return bean;
+    }
+
+    @PutMapping("/categories/{id}")
+    public Object update(Category bean, MultipartFile image, HttpServletRequest request) throws Exception {
+        String name = request.getParameter("name");
+        System.out.println("name为：" + name);
+        System.out.println("image为：" + image);
+        bean.setName(name);
+        categoryService.update(bean);
+
+        if(image!=null) {
+            saveOrUpdateImageFile(bean, image, request);
+        }
+        return bean;
     }
 }
