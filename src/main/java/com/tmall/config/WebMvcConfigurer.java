0ad80f6958d7384fadc101107669f816b0f04eb2
MercyDou <2983055254@qq.com>
2020-01-10 13:32:21 +0800

    其他拦截器

--- src/main/java/com/tmall/config/WebMvcConfigurer.java
+++ src/main/java/com/tmall/config/WebMvcConfigurer.java
@@ -1,9 +1,9 @@
 package com.tmall.config;
 
 import com.tmall.interceptor.LoginInterceptor;
+import com.tmall.interceptor.OtherInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 @Configuration
@@ -11,6 +11,11 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
     @Bean
     public LoginInterceptor getLoginIntercepter() {
         return new LoginInterceptor();
+    }
+
+    @Bean
+    public OtherInterceptor getOtherIntercepter() {
+        return new OtherInterceptor();
     }
 
     @Override
